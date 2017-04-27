78c7df9c1a1ec5bb8b1193c64e83b270a33cfb1f
Jeanie Oh <christinapark@Christinas-MacBook.local>
2017-04-27 00:34:24 -0700

    changed file import

--- Test.py
+++ Test.py
@@ -12,12 +12,12 @@ class TestStringMethods(unittest.TestCase):
         F = FileImport('test1.txtffddde')
         self.assertEqual(F.StoreFileArray(), 'No such file')
 
-    # def test_split(self):
-    #     s = 'hello world'
-    #     self.assertEqual(s.split(), ['hello', 'world'])
-    #     # check that s.split fails when the separator is not a string
-    #     with self.assertRaises(TypeError):
-    #         s.split(2)
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
 
 if __name__ == '__main__':
     unittest.main()
