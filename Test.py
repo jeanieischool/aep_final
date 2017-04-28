3c00dcf72b962ba54d7a4b30b87507a3050dd400
Jeanie Oh <christinapark@Christinas-MacBook.local>
2017-04-27 20:33:23 -0700

    changed file import

--- Test.py
+++ Test.py
@@ -14,11 +14,9 @@ class TestStringMethods(unittest.TestCase):
 
     def test_textSanitation(self):
         F = FileImport('test2.txt')
-        self.assertEqual(F.StoreFileArray(), ['','', 'hello', 'i', 'think', "you're", 'a', 'sister-in-law'])
-
-    def test_removeEmpty(self):
-        F = FileImport('test2.txt')
         self.assertEqual(F.StoreFileArray(), ['hello', 'i', 'think', "you're", 'a', 'sister-in-law'])
+
+
 
     # def test_split(self):
     #     s = 'hello world'
